3d13a2d0a9d4bace3ae61278b75ab40321b17502
QuinPoley <quin.poley@gmail.com>
2021-07-10 17:36:33 -0400

    Some janky camera movement

--- src/View.js
+++ src/View.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 import * as THREE from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 
 function View({name}) {
@@ -20,6 +21,7 @@ function View({name}) {
         // use ref as a mount point of the Three.js scene instead of the document.body
         //this.mount.appendChild( renderer.domElement );
         document.getElementById("here").appendChild(renderer.domElement);
+        document.onkeydown = keypress;
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
         var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
         var cube = new THREE.Mesh( geometry, material );
@@ -32,13 +34,71 @@ function View({name}) {
             renderer.render( scene, camera );
         };
         animate();
+        function keypress(e){
+            e = e || window.event;
+
+            if (e.keyCode == '87') {  //W
+                camera.position.y += 1;
+                console.log("Hello");
+            }
+            else if (e.keyCode == '65') { // A
+                camera.position.x -= 1;
+                console.log("Hello");
+            }
+            else if (e.keyCode == '68') { // D
+                camera.position.x += 1;
+               console.log("Hello");
+            }
+            else if (e.keyCode == '83') { //S
+                camera.position.y -= 1; 
+                console.log("Hello");
+            }
+            else if (e.keyCode == '81') { // Q
+                camera.position.z += 1;
+               console.log("Hello");
+            }
+            else if (e.keyCode == '69') { //E
+                camera.position.z -= 1; 
+                console.log("Hello");
+            }
+            //camera.updateProjectionMatrix();
+        }
         //return (renderer.domElement);
            
+    }
+
+    function ImportglHF(){
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        var renderer = new THREE.WebGLRenderer();
+        var loader = new GLTFLoader();
+        //var ExportToglTF = this
+        loader.load(
+            "Objects/ExportToglTF.glb",
+            function ( gltf ) {
+                scene.add( gltf.frame )
+                
+                /*var scale = 5.6;
+                ExportToglTF.body = gltf.scene.children[0];
+                ExportToglTF.body.name = "body";
+                ExportToglTF.body.rotation.set ( 0, -1.5708, 0 );
+                ExportToglTF.body.scale.set (scale,scale,scale);
+                ExportToglTF.body.position.set ( 0, 3.6, 0 );
+                ExportToglTF.body.castShadow = true;
+                ExportToglTF.frame = ExportToglTF.body;*/
+            }, undefined, function ( error ) {
+
+                console.error( error );
+            
+            }
+            );
+        document.getElementById("here").appendChild(renderer.domElement);
     }
 
     useEffect(() => {
         StateCount(10)
         const view = ViewportInitialize()
+        //ImportglHF()
     }, [])
     //<div className="View">{view}</div>
     return(
