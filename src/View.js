aaab20e9cbdbd0357cafc2af9882cbb7571b629e
QuinPoley <quin.poley@gmail.com>
2021-07-10 20:04:01 -0400

    Camera movement is still very janky, dont know if less or more

--- src/View.js
+++ src/View.js
@@ -12,7 +12,13 @@ function View({name}) {
     }
 
     function ViewportInitialize(){
-        
+        var leftmousedown = false;
+        var rightmousedown = false;
+        var middlemousedown = false;
+        var oldx = 0;
+        var oldy = 0;
+        var oldz = 0;
+        const gridHelper = new THREE.GridHelper( 100, 100 );
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
         var renderer = new THREE.WebGLRenderer();
@@ -22,10 +28,14 @@ function View({name}) {
         //this.mount.appendChild( renderer.domElement );
         document.getElementById("here").appendChild(renderer.domElement);
         document.onkeydown = keypress;
+        document.onmousedown = mouseisDown;
+        document.onmouseup = mouseisUp;
+        document.onmousemove = checkdrag;
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
         var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
         var cube = new THREE.Mesh( geometry, material );
         scene.add( cube );
+        scene.add( gridHelper );
         camera.position.z = 5;
         var animate = function () {
             requestAnimationFrame( animate );
@@ -34,32 +44,87 @@ function View({name}) {
             renderer.render( scene, camera );
         };
         animate();
+        function mouseisDown(e){
+            switch (e.which) {
+                case 1:
+                    leftmousedown = true;
+                    break;
+                case 2:
+                    middlemousedown = true;
+                    break;
+                case 3:
+                    rightmousedown = true;
+                    break;
+                default:
+                    alert('You have a strange Mouse!');
+            }
+        }
+        function mouseisUp(e){
+            leftmousedown = false;
+            rightmousedown = false;
+            middlemousedown = false;
+        }
+        function checkdrag(e){
+            if(leftmousedown){
+                if(e.pageX < oldx){
+                    camera.rotateY((e.pageX - oldx)/180);
+                    oldx = e.pageX;
+                }
+                else if(e.pageX > oldx){
+                    camera.rotateY((e.pageX - oldx)/180);
+                    oldx = e.pageX;
+                }
+            }
+            if(middlemousedown){
+                if(e.pageX < oldx){
+                    camera.rotateZ((e.pageX - oldx)/180);
+                    oldx = e.pageX;
+                }
+                else if(e.pageX > oldx){
+                    camera.rotateZ((e.pageX - oldx)/180);
+                    oldx = e.pageX;
+                }
+            }
+            if(rightmousedown){
+                if(e.pageY > oldy){
+                    camera.rotateX((e.pageY - oldy)/180);
+                    console.log("This third");
+                    oldy = e.pageY;
+                }
+                else if(e.pageY < oldy){
+                    camera.rotateX((e.pageY - oldy)/180);
+                    console.log("This fourth");
+                    oldy = e.pageY;
+                }
+            }
+
+        }
         function keypress(e){
             e = e || window.event;
 
             if (e.keyCode == '87') {  //W
                 camera.position.y += 1;
-                console.log("Hello");
             }
             else if (e.keyCode == '65') { // A
                 camera.position.x -= 1;
-                console.log("Hello");
             }
             else if (e.keyCode == '68') { // D
                 camera.position.x += 1;
-               console.log("Hello");
             }
             else if (e.keyCode == '83') { //S
                 camera.position.y -= 1; 
-                console.log("Hello");
             }
             else if (e.keyCode == '81') { // Q
                 camera.position.z += 1;
-               console.log("Hello");
             }
             else if (e.keyCode == '69') { //E
                 camera.position.z -= 1; 
-                console.log("Hello");
+            }
+            else if (e.keyCode == '90') { // Z
+                camera.rotateX(10/180);
+            }
+            else if (e.keyCode == '88') { // X
+                camera.rotateX(-10/180) 
             }
             //camera.updateProjectionMatrix();
         }
